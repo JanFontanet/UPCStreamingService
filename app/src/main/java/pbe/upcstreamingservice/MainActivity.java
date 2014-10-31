c7886ee143ffc26a28e6b21b1be3d804806b41a2
JanFontanet <fontanet.jan@gmail.com>
2014-10-31 19:14:48 +0100

    new

--- app/src/main/java/pbe/upcstreamingservice/MainActivity.java
+++ app/src/main/java/pbe/upcstreamingservice/MainActivity.java
@@ -2,16 +2,24 @@ package pbe.upcstreamingservice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+
+    private ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = (ListView)findViewById(android.R.id.list);
     }
 
 
